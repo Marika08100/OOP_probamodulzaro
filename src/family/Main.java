16a46072bffbf632e69a0d0955368a7abbae3a22
Marika08100 <132154696+Marika08100@users.noreply.github.com>
2023-07-03 20:02:42 +0300

    familyTree is rdy

--- src/family/Main.java
+++ src/family/Main.java
@@ -3,35 +3,25 @@ package family;
 public class Main {
     public static void main(String[] args) {
 
-
-        Person alma = new Person("Alma");
-        Person bela = new Person("Bela");
+        Person alma = new Person("Alma" );
+        Person bela = new Person("Béla");
         Person cecil = new Person("Cecil");
-        Person denes = new Person("Denes");
-        Person emoke = new Person("Emoke");
+        Person denes = new Person("Dénes");
+        Person emoke = new Person("Emőke");
         Person ferenc = new Person("Ferenc");
-        Person gyongyi = new Person("Gyongyi");
+        Person gyongyi = new Person("Gyöngyi");
 
         alma.setFather(bela);
         alma.setMother(cecil);
+        bela.setFather(cecil);
+        bela.setMother(cecil);
         cecil.setFather(denes);
         cecil.setMother(emoke);
         emoke.setFather(ferenc);
         emoke.setMother(gyongyi);
 
+
         System.out.println(alma.getFamilyTree());
-
-        Person marika = new Person("Marika");
-        Person janos = new Person("Janos");
-        Person erika = new Person("Erika");
-        Person maria = new Person("Maria");
-        Person sandor = new Person("Sandor");
-        marika.setMother(erika);
-        marika.setFather(janos);
-        erika.setFather(sandor);
-        erika.setMother(maria);
-
-        System.out.println(marika.getFamilyTree());
 
     }
 }
